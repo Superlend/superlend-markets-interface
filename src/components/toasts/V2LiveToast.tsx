9e93b873fbc5ea7124d5aca23c653ab3bf12dee0
shreyaspangal <shreyaspangal03@gmail.com>
2026-01-05 17:01:18 +0530

    fix: v2 url

--- src/components/toasts/V2LiveToast.tsx
+++ src/components/toasts/V2LiveToast.tsx
@@ -18,7 +18,7 @@ const V2LiveToast = ({ id, onDismiss }: V2LiveToastProps) => {
 
   const handleExplore = () => {
     // handleDismiss();
-    window.location.href = 'https://beta.superlend.xyz/markets';
+    window.open('https://app.superlend.xyz/markets?tab=markets', '_blank');
   };
 
   return (
